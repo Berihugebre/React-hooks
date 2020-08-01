a5c82503e30fca243a9f1caa15f496ce7b1b1e4f
berihugebre <danigebre039@gmail.com>
2020-08-01 20:30:52 +0200

    removing actions

--- expense-tracker-react/src/context/GlobalState.js
+++ expense-tracker-react/src/context/GlobalState.js
@@ -2,17 +2,12 @@ import React, {createContext, useReducer} from 'react'
 import AppReducer from '../roducer/AppReducer'
 
 // intal state
-
 const initialState ={
     transactions: [],
 }
-
 // create context
-
 export const GlobalContext = createContext(initialState)
-
 // provider component
-
 export const GlobalContextProvider =({children})=> {
     const [state, dispatch] = useReducer(AppReducer, initialState)
 
